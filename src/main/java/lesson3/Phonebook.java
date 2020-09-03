58690f37193ef3a966c75eaab2c9284cdc8187e7
Никита <matskenik@gmail.com>
2020-09-03 19:22:58 +0300

    рефакторинг по итогам разбора дз

--- src/main/java/lesson3/Phonebook.java
+++ src/main/java/lesson3/Phonebook.java
@@ -4,29 +4,24 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Phonebook {
-    private final HashMap<String, HashSet<String>> phonebook;
-
-    public Phonebook() {
-        this.phonebook = new HashMap<>();
-    }
+    private final HashMap<String, HashSet<String>> phonebook = new HashMap<>();
 
     public void add(String lastname, String phone) {
         HashSet<String> phones;
-        if(this.phonebook.containsKey(lastname)) {
-            phones = this.phonebook.get(lastname);
+        if(phonebook.containsKey(lastname)) {
+            phones = phonebook.get(lastname);
         } else {
             phones = new HashSet<>();
         }
         phones.add(phone);
-        this.phonebook.put(lastname, phones);
+        phonebook.put(lastname, phones);
     }
 
-    public void get(String lastname) {
-        if(this.phonebook.containsKey(lastname)) {
-            System.out.println(this.phonebook.get(lastname));
+    public HashSet<String> get(String lastname) {
+        if(phonebook.containsKey(lastname)) {
+            return phonebook.get(lastname);
         } else {
-            System.out.printf("В справочнике нет фамилии - %s!%n", lastname);
+            return new HashSet<>();
         }
-
     }
 }
